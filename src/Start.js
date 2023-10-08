b02556ac53de167ccbf3d0c92d7495cc5ec3f34b
TobCraft3521 <tobiashackenberg@outlook.de>
2023-10-08 03:24:37 +0200

    partly implemented openfile dialog

--- src/Start.js
+++ src/Start.js
@@ -1,22 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 export function Start({ handleData }) {
-    const handleOpenFile = () => {
+    const handleFile = (e) => {
 
     }
+
     return (
         <div>
             <h1>Start</h1>
             <br />
 
             <Link style={{ color: "white" }} to="/viewpresets">  <div className="button-85">Load preset </div></Link>
-
-            <div className="button-85" onClick={handleOpenFile}>
+            <div className="button-85" onClick={() => { document.getElementById("fileselector").click() }}>
                 Open from file
+                <input type="file" id="fileselector" style={{ display: "none" }} onChange={handleFile}/>
             </div>
-
             <Link style={{ color: "white" }} to="/view">    <div className="button-85">Create new set </div></Link>
-
         </div>
     )
 }
