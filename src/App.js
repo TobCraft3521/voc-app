fe66f8cae1f26d19eb1ec4c8a6e8cf9034413e44
TobCraft3521 <tobiashackenberg@outlook.de>
2023-10-08 08:12:23 +0200

    Saving and opening fully functional

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
     description: "Empty vocabulary dataset"
   })
 
-  const handleData = (data, imgs, title, description) => {
-    setSet({ a: data, b: {}, imgs, title, description })
+  const handleData = (data) => {
+    setSet({ a: data.a, b: {}, imgs: data.imgs, title: data.title, description: data.description })
   }
 
   useEffect(() => {
@@ -35,8 +35,8 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Start />} />
-            <Route path="/open" element={<Start />} />
+            <Route path="/" element={<Start handleData={handleData} />} />
+            <Route path="/open" element={<Start handleData={handleData} />} />
             <Route path='/view' element={<View set={set} setSet={setSet} />} />
             <Route path='/edit' element={<Edit onChange={handleData} set={set.a} imgs={set.imgs} title={set.title} description={set.description} />} />
             <Route path='/viewpresets' element={<ViewPresets />} />
--- src/Start.js
+++ src/Start.js
@@ -2,6 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 export function Start({ handleData }) {
     const handleFile = (e) => {
+        const file = e.target.files[0]
+        if (!file.name.endsWith(".json")) { alert("Please select a valid file with a .json ending."); return }
+        const reader = new FileReader()
+        let content = ""
+        reader.onload = (ev) => {
+            content = ev.target.result
+            console.log(content)
+            const json = JSON.parse(content)
+            handleData(json)
+        }
+        reader.readAsText(file, "UTF-8")
 
     }
 
@@ -13,9 +24,9 @@ export function Start({ handleData }) {
             <Link style={{ color: "white" }} to="/viewpresets">  <div className="button-85">Load preset </div></Link>
             <div className="button-85" onClick={() => { document.getElementById("fileselector").click() }}>
                 Open from file
-                <input type="file" id="fileselector" style={{ display: "none" }} onChange={handleFile}/>
+                <input type="file" id="fileselector" style={{ display: "none" }} onChange={handleFile} />
             </div>
-            <Link style={{ color: "white" }} to="/view">    <div className="button-85">Create new set </div></Link>
+            <Link style={{ color: "white" }} to="/view"> <div className="button-85">Create new set </div> </Link>
         </div>
     )
 }
